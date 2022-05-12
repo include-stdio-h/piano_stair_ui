2110141e703a44aebf05b191f33106c788a75f09
MoonSeoJun <msj00130@gmail.com>
2022-05-12 19:04:39 +0900

    [INIT] GUI init

--- src/main.py
+++ src/main.py
@@ -1,1 +1,23 @@
-print()
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication
+
+from ui.piano_stairs_design import Ui_MainWindow
+
+import sys
+
+class PianoStairUI(QtWidgets.QMainWindow,Ui_MainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QMainWindow.__init__(self, parent=parent)
+        self.setupUi(self)
+        super().__init__()
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+
+    piano_stair = PianoStairUI()
+    piano_stair.setupUi(MainWindow)
+
+    MainWindow.show()
+
+    sys.exit(app.exec_())
