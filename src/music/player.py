11278555e50e11bee57cb5be0ff7230b41e91190
MoonSeoJun <msj00130@gmail.com>
2022-05-30 21:50:55 +0900

    TEst

--- src/music/player.py
+++ src/music/player.py
@@ -22,8 +22,11 @@ def select_instrument(instrument_num):
 
 def music_player(socket_lst, key_index, test_lst):
     while True:
+        print("!!!!!")
         if test_lst[key_index] == 0:
+            print("??????")
             if socket_lst[key_index*2+1] == '1':
+                print("&&&&&&")
                 print(music_keys[key_index])
                 # channel_lst[key_index].play(key_lst[key_index])
                 test_lst[key_index] = 1
