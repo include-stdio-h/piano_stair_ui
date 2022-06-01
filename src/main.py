19e8635f48016b6468350723ea41d464e0daeeff
MoonSeoJun <msj00130@gmail.com>
2022-06-01 15:49:29 +0900

    design init

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtCore import Qt
 
 import sys
 import threading
@@ -9,27 +8,20 @@ from style.style_handler import StyleHandler
 from style.animation.menu import MenuAnimation
 from style.animation.theme import ThemeAnimation
 from network.serial_connect import serial_socket
-from music.player import select_instrument
 from constants import ( 
     BLUETOOTH_PAGE_INDEX, 
     INSTRUMENT_PAGE_INDEX,
     SELECTED_INSTRUMENT_STYLE, 
     UNSELECTED_INSTRUMENT_STYLE,
-    INSTRUMENTS_THEME,
-    GRAND_PIANO_INDEX,
-    HARP_INDEX,
-    ORGAN_INDEX,
-    GUITAR_INDEX,
-    ACCORDION_INDEX,
-    VIBRA_PHONE_INDEX, 
+    INSTRUMENTS_THEME
 )
+
 
 class PianoStairUI(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         QtWidgets.QMainWindow.__init__(self, parent=parent)
         self.setupUi(self)
         super().__init__()
-        self.setWindowFlag(Qt.WindowType.FramelessWindowHint)
 
     def variable_init(self):
         self.instrument_backs = [
@@ -48,7 +40,7 @@ class PianoStairUI(QtWidgets.QMainWindow, Ui_MainWindow):
         ]
 
         self.selected_menu_animation = MenuAnimation(self.SelectedMenu, b"pos")
-        self.change_instrument_theme_animation = ThemeAnimation(self.InstrumentTheme, b"geometry")
+        self.change_instrumenttheme_animation = ThemeAnimation(self.InstrumentTheme, b"geometry")
         self.change_total_theme_animation = ThemeAnimation(self.TotalTheme, b"geometry")
 
         self.before_instrument = self.BassGuitarBack
@@ -65,34 +57,33 @@ class PianoStairUI(QtWidgets.QMainWindow, Ui_MainWindow):
         self.ToolBar = StyleHandler.add_shadow(self.ToolBar)
 
     def signal_init(self):
-        self.BassGuitarSelect.clicked.connect(lambda : self.change_instrument(self.BassGuitarBack, INSTRUMENTS_THEME["bass_guitar"], GUITAR_INDEX))
-        self.OrganSelect.clicked.connect(lambda : self.change_instrument(self.OrganBack, INSTRUMENTS_THEME["organ"], ORGAN_INDEX))
-        self.AccordionSelect.clicked.connect(lambda : self.change_instrument(self.AccordionBack, INSTRUMENTS_THEME["accordion"], ACCORDION_INDEX))
-        self.PianoSelect.clicked.connect(lambda : self.change_instrument(self.PianoBack, INSTRUMENTS_THEME["piano"], GRAND_PIANO_INDEX))
-        self.HarpSelect.clicked.connect(lambda : self.change_instrument(self.HarpBack, INSTRUMENTS_THEME["harp"], HARP_INDEX))
-        self.VibraPhoneSelect.clicked.connect(lambda : self.change_instrument(self.VibraPhoneBack, INSTRUMENTS_THEME["vibra_phone"], VIBRA_PHONE_INDEX))
+        self.BassGuitarSelect.clicked.connect(lambda : self.change_instrument(self.BassGuitarBack, INSTRUMENTS_THEME["bass_guitar"]))
+        self.OrganSelect.clicked.connect(lambda : self.change_instrument(self.OrganBack, INSTRUMENTS_THEME["organ"]))
+        self.AccordionSelect.clicked.connect(lambda : self.change_instrument(self.AccordionBack, INSTRUMENTS_THEME["accordion"]))
+        self.PianoSelect.clicked.connect(lambda : self.change_instrument(self.PianoBack, INSTRUMENTS_THEME["piano"]))
+        self.HarpSelect.clicked.connect(lambda : self.change_instrument(self.HarpBack, INSTRUMENTS_THEME["harp"]))
+        self.VibraPhoneSelect.clicked.connect(lambda : self.change_instrument(self.VibraPhoneBack, INSTRUMENTS_THEME["vibra_phone"]))
 
         self.QuitButton.clicked.connect(sys.exit)
 
         self.BlueToothButton.clicked.connect(lambda : self.change_page(BLUETOOTH_PAGE_INDEX))
         self.InstrumentButton.clicked.connect(lambda : self.change_page(INSTRUMENT_PAGE_INDEX))
 
-        self.change_instrument_theme_animation.SelectedMenuAnimation.finished.connect(lambda : self.WorkSpace.setStyleSheet(self.current_theme))
+        self.change_instrumenttheme_animation.SelectedMenuAnimation.finished.connect(lambda : self.WorkSpace.setStyleSheet(self.current_theme))
         self.change_total_theme_animation.SelectedMenuAnimation.finished.connect(lambda : self.Background.setStyleSheet(self.current_theme))
 
     def change_page(self, page_index):
         self.selected_menu_animation.updown_animation(page_index)
         self.WorkSpace.setCurrentIndex(page_index)
 
-    def change_instrument(self, now, widget_theme, instrument_num):
-        select_instrument(instrument_num)
-        self.change_instrument_theme_animation.change_theme(widget_theme, "instrument")
+    def change_instrument(self, now, widget_theme):
+        self.change_instrumenttheme_animation.change_theme(widget_theme, "instrument")
         self.change_total_theme_animation.change_theme(widget_theme, "total")
 
         self.current_theme = widget_theme["style"]
 
-        self.before_instrument = StyleHandler.change_style(self.before_instrument, UNSELECTED_INSTRUMENT_STYLE)
-        now = StyleHandler.change_style(now, SELECTED_INSTRUMENT_STYLE)
+        self.before_instrument = StyleHandler.change_theme(self.before_instrument, UNSELECTED_INSTRUMENT_STYLE)
+        now = StyleHandler.change_theme(now, SELECTED_INSTRUMENT_STYLE)
 
         self.before_instrument = now
         self.selected_instrument = self.instrument_backs.index(now) + 1
@@ -109,7 +100,7 @@ if __name__ == '__main__':
     piano_stair.design_init()
     piano_stair.signal_init()
 
-    th = threading.Thread(target=serial_socket, args=(piano_stair, ))
+    th = threading.Thread(target=serial_socket, args=piano_stair)
     th.daemon = True
     th.start()
 
