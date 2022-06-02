afbfb3f5b6d6a81d224442d4a9f0032006efabb6
MoonSeoJun <msj00130@gmail.com>
2022-06-02 15:29:01 +0900

    Modify main page

--- src/ui/piano_stairs_design.py
+++ src/ui/piano_stairs_design.py
@@ -683,7 +683,7 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
-        self.WorkSpace.setCurrentIndex(2)
+        self.WorkSpace.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
